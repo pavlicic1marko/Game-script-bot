de84ce42a6db67011d30211ecafae5b9830c778d
MP <pavlicic1marko@gmail.com>
2024-11-18 21:37:24 +0100

    use MouseCoordinates.py  to find the UI element coordinates

---
+++ MouseCordinates.py
@@ -0,0 +1,12 @@
+import time
+
+import pyautogui, sys
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr)
+        time.sleep(2)
+except KeyboardInterrupt:
+    print('\n')
