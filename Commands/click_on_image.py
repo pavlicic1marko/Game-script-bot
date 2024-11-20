f8209dc4a26e6f82d8838f63bb9366b3226aae00
MP <pavlicic1marko@gmail.com>
2024-11-20 18:34:13 +0100

    refactor, to click with pixel variation

--- Commands/click_on_image.py
+++ Commands/click_on_image.py
@@ -9,11 +9,12 @@ region = (left, top, wight, height)  # game screen dimensions
 def click_on_image(image_name):
     image_cordinates = pyautogui.locateOnScreen('C:/Users/MP/PycharmProjects/LastWarautomation/images/'+ image_name,
     region=region, confidence=0.8, grayscale=True)
-    click_with_random_sleep(image_cordinates)
+    x,y = pyautogui.center(image_cordinates)
+    click_with_random_sleep_and_cordinate_variation([x,y])
+
 
 def click_on_exact_image(image_name):
-    image_cordinates = pyautogui.locateOnScreen('C:/Users/MP/PycharmProjects/LastWarautomation/images/' + image_name,
-                                                region=region, confidence=1, grayscale=True)
+    image_cordinates = pyautogui.locateOnScreen('C:/Users/MP/PycharmProjects/LastWarautomation/images/' + image_name,                                               region=region, confidence=1, grayscale=True)
     click_with_random_sleep(image_cordinates)
 
 
