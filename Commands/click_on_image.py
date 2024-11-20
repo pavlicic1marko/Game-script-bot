d8adfcd9c3084cafe3c6cd552dc451f1ee531bdc
MP <pavlicic1marko@gmail.com>
2024-11-20 17:47:06 +0100

    refactor as OOP

---
+++ Commands/click_on_image.py
@@ -0,0 +1,19 @@
+from Commands.Click import *
+
+top = 0
+left = 655
+height = 1079
+wight = 609
+region = (left, top, wight, height)  # game screen dimensions
+
+def click_on_image(image_name):
+    image_cordinates = pyautogui.locateOnScreen('C:/Users/MP/PycharmProjects/LastWarautomation/images/'+ image_name,
+    region=region, confidence=0.8, grayscale=True)
+    click_with_random_sleep(image_cordinates)
+
+def click_on_exact_image(image_name):
+    image_cordinates = pyautogui.locateOnScreen('C:/Users/MP/PycharmProjects/LastWarautomation/images/' + image_name,
+                                                region=region, confidence=1, grayscale=True)
+    click_with_random_sleep(image_cordinates)
+
+
---
+++ POM/click on help allies image.py
@@ -0,0 +1,4 @@
+from Commands.click_on_image import click_on_image
+
+click_on_image('hellp_allies_button.png')
+
