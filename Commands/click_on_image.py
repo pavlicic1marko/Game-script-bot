8831fbdb634ee88b7d90e65fe698991f1378acd8
MP <pavlicic1marko@gmail.com>
2024-11-23 19:43:50 +0100

    fix image path location, to be relative not hardcoded

--- Commands/click_on_image.py
+++ Commands/click_on_image.py
@@ -1,4 +1,5 @@
 from Commands.Click import *
+from images.images_location import get_image_folder_path
 
 top = 0
 left = 655
@@ -6,15 +7,18 @@ height = 1079
 wight = 609
 region = (left, top, wight, height)  # game screen dimensions
 
+image_folder_path = get_image_folder_path()
+
 def click_on_image(image_name):
-    image_cordinates = pyautogui.locateOnScreen('C:/Users/MP/PycharmProjects/LastWarautomation/images/'+ image_name,
+    image_cordinates = pyautogui.locateOnScreen(image_folder_path + '\\' + image_name,
     region=region, confidence=0.8, grayscale=True)
     x,y = pyautogui.center(image_cordinates)
     click_with_random_sleep_and_cordinate_variation([x,y])
 
 
 def click_on_exact_image(image_name):
-    image_cordinates = pyautogui.locateOnScreen('C:/Users/MP/PycharmProjects/LastWarautomation/images/' + image_name,                                               region=region, confidence=1, grayscale=True)
+    image_cordinates = pyautogui.locateOnScreen(image_folder_path + '\\' + image_name,
+    region=region, confidence=1, grayscale=True)
     click_with_random_sleep(image_cordinates)
 
 
