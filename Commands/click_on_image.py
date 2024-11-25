d93327507256a155d4252f23a1bcbaf118ecca7f
MP <pavlicic1marko@gmail.com>
2024-11-26 00:21:40 +0100

    refactor image path

--- Commands/click_on_image.py
+++ Commands/click_on_image.py
@@ -1,5 +1,5 @@
 from Commands.Click import *
-from images.images_location import get_image_folder_path
+from images_location import get_image_folder_path
 
 top = 0
 left = 655
@@ -7,17 +7,17 @@ height = 1079
 wight = 609
 region = (left, top, wight, height)  # game screen dimensions
 
-image_folder_path = get_image_folder_path()
+
 
 def click_on_image(image_name):
-    image_cordinates = pyautogui.locateOnScreen(image_name,
+    image_cordinates = pyautogui.locateOnScreen(get_image_folder_path()+image_name,
     region=region, confidence=0.8, grayscale=True)
     x,y = pyautogui.center(image_cordinates)
     click_with_random_sleep_and_cordinate_variation([x,y])
 
 
 def click_on_exact_image(image_name):
-    image_cordinates = pyautogui.locateOnScreen(image_folder_path + '\\' + image_name,
+    image_cordinates = pyautogui.locateOnScreen(get_image_folder_path() + image_name,
     region=region, confidence=1, grayscale=True)
     click_with_random_sleep(image_cordinates)
 
--- Scripts/click_on_help_allies_image.py
+++ Scripts/click_on_help_allies_image.py
@@ -1,13 +1,14 @@
 import time
 import pyautogui
 from Commands.click_on_image import click_on_image
+from images_location import get_image_folder_path
 
 
 def help_allies():
     number_of_clicks = 0
     while True:
         try:
-            click_on_image('images/hellp_allies_button.png')
+            click_on_image('\\images\\hellp_allies_button.png')
             number_of_clicks += 1
             print("clicked: " + str(number_of_clicks))
         except pyautogui.ImageNotFoundException:
