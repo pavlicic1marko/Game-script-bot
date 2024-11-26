994220e42a70105b2cbd0ff4dc4e18726b5e506c
MP <pavlicic1marko@gmail.com>
2024-11-26 02:16:26 +0100

    refactor image path to work with .exe file

--- Commands/click_on_image.py
+++ Commands/click_on_image.py
@@ -1,5 +1,7 @@
 from Commands.Click import *
 from images_location import get_image_folder_path
+import os
+import sys
 
 top = 0
 left = 655
@@ -7,10 +9,18 @@ height = 1079
 wight = 609
 region = (left, top, wight, height)  # game screen dimensions
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS2 # or 2
+    except Exception:
+        base_path = os.path.abspath(".")
 
+    return os.path.join(base_path, relative_path)
 
 def click_on_image(image_name):
-    image_cordinates = pyautogui.locateOnScreen(get_image_folder_path()+image_name,
+    image_cordinates = pyautogui.locateOnScreen(resource_path(image_name),
     region=region, confidence=0.8, grayscale=True)
     x,y = pyautogui.center(image_cordinates)
     click_with_random_sleep_and_cordinate_variation([x,y])
