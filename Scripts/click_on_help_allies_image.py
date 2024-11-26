fa64acd7a4086b72ec5722b51bb047fcbfe90a74
MP <pavlicic1marko@gmail.com>
2024-11-26 12:36:25 +0100

    login path refactor for .exe app

--- Scripts/click_on_help_allies_image.py
+++ Scripts/click_on_help_allies_image.py
@@ -1,9 +1,21 @@
+import os
+import sys
 import time
 import pyautogui
 from Commands.click_on_image import click_on_image
 import logging
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='logs/myapp.log', level=logging.INFO)
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS2 # or 2
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+logging.basicConfig(filename=resource_path('logs\\myapp.log'), level=logging.INFO)
 
 
 def help_allies():
