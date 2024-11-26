af9e9a712bbb75b786151e6243a8c13ba6e61b88
MP <pavlicic1marko@gmail.com>
2024-11-26 12:29:28 +0100

    test login

--- Scripts/click_on_help_allies_image.py
+++ Scripts/click_on_help_allies_image.py
@@ -1,13 +1,16 @@
 import time
 import pyautogui
 from Commands.click_on_image import click_on_image
-from images_location import get_image_folder_path
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='logs/myapp.log', level=logging.INFO)
 
 
 def help_allies():
     number_of_clicks = 0
     while True:
         try:
+            logger.info('Started help_allies script')
             click_on_image('images\\hellp_allies_button.png')
             number_of_clicks += 1
             print("clicked: " + str(number_of_clicks))
