3efb08c40ffc7ec9653274bff128c3f8ec41438c
MP <pavlicic1marko@gmail.com>
2024-11-27 15:58:03 +0100

    refactor images

--- Scripts/approve_all_roles_using_images.py
+++ Scripts/approve_all_roles_using_images.py
@@ -1,25 +1,29 @@
+import time
 import pyautogui
-
 from Commands.click_on_image import click_on_image, click_on_image_if_visible
 
 
 def approve_all_5_roles():
-    approve_role('secretary_of_strategy.PNG')
-    approve_role('secretary_of_security.PNG')
-    approve_role('secretary_of_development.PNG')
-    approve_role('secretary_of_science.PNG')
-    approve_role('secretary_of_interior.PNG')
+    approve_role('secretary_of_strategy.png')
+    approve_role('secretary_of_security.png')
+    approve_role('secretary_of_development.png')
+    approve_role('secretary_of_science.png')
+    approve_role('secretary_of_interior.png')
+    time.sleep(5)
 
 
 def approve_role(role_name):
-    click_on_image('images\\' + role_name)
-    click_on_image('images\\list_button.PNG')
-    click_on_image_if_visible('images\\Approve_from_list_button.PNG')
-    click_on_image('images\\close.PNG')
+    click_on_image('images\\maximise_screen\\' + role_name)
+    click_on_image('images\\maximise_screen\\list_button.png')
+    click_on_image_if_visible('images\\maximise_screen\\Approve_from_list_button.png')
+    click_on_image('images\\maximise_screen\\close.PNG')
     pyautogui.moveTo(400, 400)
-    click_on_image('images\\close.PNG')
+    click_on_image('images\\maximise_screen\\close.png')
 
 
 if __name__ == "__main__":
+    i = 0
     while True:
         approve_all_5_roles()
+        i +=1
+        print(i)
