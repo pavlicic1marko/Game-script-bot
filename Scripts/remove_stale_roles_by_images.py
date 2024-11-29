f50c96ce1a7793bd0c9d27fbd1433a820f54ea45
MP <pavlicic1marko@gmail.com>
2024-11-29 11:42:03 +0100

    test time using ocr for all 5 roles, refactor

--- Scripts/remove_stale_roles_by_images.py
+++ Scripts/remove_stale_roles_by_images.py
@@ -17,20 +17,9 @@ image_folder_maximize="images\\maximise_screen\\"
 reader = easyocr.Reader(['en'])
 
 def time_to_minutes(time_str):
-    hours, minutes, _ = map(int, time_str.split('.'))
+    hours, minutes, _ = map(int, time_str.split(':'))
     return hours * 60 + minutes
 
-def text_sanitization(time_str):
-    print("timme:" + time_str)
-
-    if not time_str:
-        return ''
-    if time_str[:3].isdigit():
-        time_str = time_str[1:]  # Remove extra leading digit
-    parts = time_str.split(':')
-    if len(parts[2]) > 2:
-        parts[2] = parts[2][:2]  # Remove extra trailing digit
-    return ':'.join(parts)
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -44,8 +33,9 @@ def resource_path(relative_path):
 
 def remove_stale_user(role_image):
     click_on_image(role_image)
+    time.sleep(1)
     image_cordinates = pyautogui.locateOnScreen(resource_path(image_folder_maximize + 'time_in_office_text.png'),
-    region=region, confidence=0.8, grayscale=True)
+    region=region, confidence=0.7, grayscale=True)
     print(image_cordinates)
 
     region2 = (int(image_cordinates.left)+image_cordinates.width, int(image_cordinates.top), image_cordinates.width, image_cordinates.height)
@@ -53,15 +43,18 @@ def remove_stale_user(role_image):
     # Capture the screen region
     screenshot = pyautogui.screenshot(region=region2)
     # read text from immage
-
-    time_on_screen = reader.readtext(np.array(screenshot))[0][1]
-    time_on_screen = time_on_screen.replace(":",".")
-    time_on_screen = time_on_screen.replace(",",".")
-    time_on_screen = time_on_screen.replace(";",".")
-    time_on_screen = time_on_screen.replace("'",".")
+    try:
+        time_on_screen = reader.readtext(np.array(screenshot))[0][1]
+    except IndexError:
+        click_on_image('close.PNG')
 
 
-
+    time_on_screen = time_on_screen.replace(".",":")
+    time_on_screen = time_on_screen.replace(",",":")
+    time_on_screen = time_on_screen.replace(";",":")
+    time_on_screen = time_on_screen.replace("'",":")
+    pattern = r'\b\d{2}:\d{2}:\d{2}\b'
+    matches = re.findall(pattern, time_on_screen)
 
     # save screenshot for debugging purpose
     screenshot.save( " screenshot.png")
@@ -71,11 +64,8 @@ def remove_stale_user(role_image):
 
 
 
-    print(time_on_screen)
-    if not time_on_screen:
-        print("the bot did not find the time")
-
-    else:
+    print(matches)
+    if  matches:
         minutes = time_to_minutes(time_on_screen)
         print("the number of minutes is: " + str(minutes))
 
@@ -85,14 +75,24 @@ def remove_stale_user(role_image):
             print("time is less than 6 minutes")
         click_on_image('close.PNG')
 
+    else:
+        print("the bot did not find the time")
+        click_on_image('close.PNG')
+
+
+
+
 
 
 if __name__ == "__main__":
+    i=1
     while True:
+        i+=1
         remove_stale_user('secretary_of_strategy.png')
         remove_stale_user('secretary_of_security.png')
         remove_stale_user('secretary_of_development.png')
         remove_stale_user('secretary_of_science.png')
         remove_stale_user('secretary_of_interior.png')
+        print(i)
         time.sleep(5)
 
