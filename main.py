2f1555d0a65c0339112595456c21cf4fc3dfcbc3
MP <pavlicic1marko@gmail.com>
2024-12-05 16:54:46 +0100

    changing messages , button names, title , resize window, adding messages

--- main.py
+++ main.py
@@ -1,47 +1,60 @@
 import tkinter as tk
 from tkinter import messagebox
 import multiprocessing
-import os
 from Scripts.click_on_help_allies_image import help_allies
+
+window_title = "First Lady Approve Bot"
 
 
 class ProcessControllerApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Process Controller")
+        self.root.title(window_title)
         self.process = None
 
+        # app description
+        self.app_label = tk.Label(root, text="Bot approves users in list for all 5 roles", fg='blue', font=('Arial', 11))
+        self.app_label.pack()
+
         # Create buttons
-        self.start_button = tk.Button(root, text="Start Process", command=self.start_process)
+        self.start_button = tk.Button(root, text="Start FL Bot", command=self.start_process)
         self.start_button.pack(pady=10)
 
-        self.stop_button = tk.Button(root, text="Stop Process", command=self.stop_process)
+        self.stop_button = tk.Button(root, text="Stop FL Bot", command=self.stop_process)
         self.stop_button.pack(pady=10)
 
+        # app warning
+        self.app_label = tk.Label(root, text="To stop the bot move mouse to the corner of your screen", fg='red', font=('Arial', 12))
+        self.app_label.pack(pady=20)
+
+
+
     def start_process(self):
-        """Starts a new process running example_function."""
+        """Starts a new process running target function."""
         if self.process and self.process.is_alive():
-            messagebox.showinfo("Info", f"Process is already running with PID {self.process.pid}.")
+            messagebox.showinfo("Info", f"FL Bot is already running, PID: {self.process.pid}.")
             return
 
         self.process = multiprocessing.Process(target=help_allies)
         self.process.start()
-        messagebox.showinfo("Info", f"Started process with PID {self.process.pid}.")
+        messagebox.showinfo("Info", "Started FL Bot.")
 
     def stop_process(self):
         """Terminates the process using its PID."""
         if not self.process or not self.process.is_alive():
-            messagebox.showinfo("Info", "No process is currently running.")
+            messagebox.showinfo("Info", "FL Boot is currently not running.")
             return
 
         # Terminate the process
         self.process.terminate()
         self.process.join()  # Wait for the process to finish cleanup
-        messagebox.showinfo("Info", f"Terminated process with PID {self.process.pid}.")
+        messagebox.showinfo("Info", "Stopped FL Bot ")
         self.process = None  # Reset the process
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = ProcessControllerApp(root)
+    canvas = tk.Canvas(root, width=600, height=100)
+    canvas.pack()
     root.mainloop()
